3e4b628befb17f3f67fc3bad83958feca726a94f
Llewellyn Falco <llewellyn.falco@gmail.com>
2020-11-09 11:47:43 -0700

    Create install.windows.ps1

---
+++ install.windows.ps1
@@ -0,0 +1,15 @@
+# To run this script directly, run this in an elevated admin PowerShell prompt:
+#     Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/approvals/ApprovalTests.js.StarterProject/master/install.windows.ps1 | Invoke-Expression
+
+# This script is intended to setup a dev machine from scratch. Very useful for setting up a EC2 instance for mobbing.
+#
+
+iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | iex
+iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/javascript-webstorm.ps1 | iex
+
+# Clone repo
+& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/approvals/ApprovalTests.js.StarterProject.git C:\Code\ApprovalTests.js.StarterProject
+
+# Done
+cls
+echo "Done!"
